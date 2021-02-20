fdde6845f8facccf190686dde8027392f92d2462
mastevb <bochenma@cs.washington.edu>
2021-02-20 12:57:34 -0800

    method definition

--- productinfo/service/productinfo_service.go
+++ productinfo/service/productinfo_service.go
@@ -10,3 +10,15 @@ import (
 )
 
 // server is used to implement ecommerce/product_info
+type server struct {
+	productMap map[string]*pb.Product
+}
+
+// AddProduct implements ecommerce.AddProduct
+func (s *server) AddProduct(ctx context.Context, in *pb.Product) (*pb.ProductID, error) {
+
+}
+
+func (s *server) GetProduct(ctx context.Context, in *pb.ProductID) (*pb.Product, error) {
+
+}
